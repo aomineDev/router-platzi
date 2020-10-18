f5797ec7cb2db43eab07f8db062ddd9b74601cd7
aomine <aomine1745@gmail.com>
2020-10-17 22:12:15 -0500

    course finished

---
+++ router-spa/js/routes.js
@@ -0,0 +1,14 @@
+const routes = [
+  {
+    path: '/',
+    template: /* html */ `<h1>Hola</h1>`
+  },
+  {
+    path: '/contact',
+    template: /* html */ `<h1>Contact</h1>`
+  },
+  {
+    path: '/about-me',
+    template: /* html */ `<h1>About me</h1>`
+  }
+]
